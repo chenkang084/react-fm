e70e36a3dcfc89a6ca92501b6034cd609618c4a4
chenkang1 <chenkang1@lenovo.com>
2017-06-03 21:23:10 +0800

    add files

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,33 +1,9 @@
-// import { combineReducers } from 'redux';
-import objectAssign from 'object-assign';
-import { FETCH_ISSUES, RECEIVE_ISSUES } from '../constants/ActionTypes.js';
-import { fetchIssues, receiveIssues } from '../actions/index.js';
+/**
+ * Created by chenkang1 on 2017/6/3.
+ */
+import {combineReducers} from 'redux'
 
-var defaultIssuesState1 = {
-  isFetching: false,
-  items: []
-};
 
-// issues reducer
-function postIssues(defaultIssuesState, action) {
-  switch (action.type) {
-    case FETCH_ISSUES:
-      // 获取issues
-      return objectAssign({}, defaultIssuesState, {
-        isFetching: true
-      });
+export default combineReducers({
 
-    case RECEIVE_ISSUES:
-      // 接收issues
-      return objectAssign({}, defaultIssuesState, {
-        isFetching: false,
-        items: action.posts
-      });
-
-    default:
-      return defaultIssuesState;
-  }
-}
-
-export default postIssues;
-
+})
